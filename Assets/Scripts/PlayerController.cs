71a8522a3452b80dfccf68ece1973921c3c43c33
Void <daigoukaruna@gmail.com>
2019-10-14 11:48:08 +0100

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     //Component Variables
     private Rigidbody2D rig;
     private AudioSource source;
+    private Animator anim;
 
     //Player Speeds
     public float moveSpeed = 150f;
@@ -15,6 +16,8 @@ public class PlayerController : MonoBehaviour
 
     //Other variables
     private int points = 0;
+    private int lives = 3;
+    private float lastDirectionPressed = 1;
 
 
     //Initializes when the script starts
@@ -23,7 +26,7 @@ public class PlayerController : MonoBehaviour
         //Get Components
         rig = GetComponent<Rigidbody2D>();
         source = GetComponent<AudioSource>();
-
+        anim = GetComponent<Animator>();
     }
 
     //Player Input
@@ -33,11 +36,19 @@ public class PlayerController : MonoBehaviour
         {
             rig.AddForce(new Vector2(0, jumpSpeed)); //...Add a vertical force to the model
             isGrounded = false;
+            anim.Play("Jumping"); //And animate the model
         }
 
         if (Input.GetAxis("Horizontal") != 0) //If the player presses any of the horizontal axis buttons...
         {
             rig.velocity = (new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rig.velocity.y)); //...move the model to the direction the player presses, while retaining the vertical velocity of the rigidbody
+            anim.Play("Moving"); //And animate the model
+        }
+
+        if (Input.GetAxisRaw("Horizontal") != 0 && Input.GetAxisRaw("Horizontal") != lastDirectionPressed)
+        {
+            Flip();
+            lastDirectionPressed = Input.GetAxisRaw("Horizontal");
         }
     }
 
@@ -48,12 +59,6 @@ public class PlayerController : MonoBehaviour
         if (col.gameObject.tag == "Ground")
         {
             isGrounded = true;
-        }
-
-        //Death
-        if (col.gameObject.tag == "Enemy")
-        {
-            Destroy(gameObject);
         }
     }
 
@@ -67,5 +72,34 @@ public class PlayerController : MonoBehaviour
             Debug.Log(points);
             Destroy(trigger.gameObject);
         }
+
+        //Adding HP
+        if (trigger.gameObject.tag == "Heal")
+        {
+            Destroy(trigger.gameObject);
+            lives += 1;
+            Debug.Log(lives);
+        }
+
+        //Death
+        if (trigger.gameObject.tag == "Enemy" && lives > 0)
+        {
+            Destroy(trigger.gameObject);
+            lives -= 1;
+            Debug.Log(lives);
+        }
+        else if (trigger.gameObject.tag == "Enemy" && lives == 0)
+        {
+            Destroy(gameObject);
+            Debug.Log(lives);
+        }
+    }
+
+    //Method to use to flip the player sprite
+    void Flip()
+    {
+        Vector3 Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
     }
 }
