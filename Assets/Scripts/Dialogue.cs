d7d2569349cd6f54c57ad0deeda781d666ed8ecd
Void <daigoukaruna@gmail.com>
2019-10-18 10:50:11 +0100

    writing some dialogue

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //  Prologue.
+    //
+
+    //  1. golden spatula gets stolen by ricardo
+    //
+
+    //  2. gonzalo tells papa alfonso
+    //G: ...!
+    //G: Papa Alfonso! Mamma mia, it really is you!
+    //A: ...Huh?? Who's there?
+    //G: Papa, it's me! Your son Gonzalo! You showed up at the right time, I really need your advice!
+    //A: Whatsa say sonnie??
+    //G: Do you remember papa? When I was younger and you'd teach me all kinds of tricks in the kitchen? I wanted to make you feel proud of me papa...
+    //G: I won the Golden Spatula papa, but that wicked Ricardo stole it away from me!
+    //G: I don't know what to do now, he's probably celebrating the success of his evil plan!
+    //A: A little bit louder sonnie!
+    //G: ...You're right.
+    //G: I shouldn't sit here and weep over my loss. I should go take it back from him! At all costs!
+    //G: Thank you, papa Alfonso. You're my hero.
+    //G: I'm going to take the Golden Spatula back and make you proud!
+    //A: ...
+    //A: ...What a weird kiddo.
+
+    //  3. gonzalo mini-game?
+    //
+
+    //  4. gonzalo drops his keys in salsa
+    //
+
+    //  5. boss fight vs ricardo
+    //G: This is it, Ricardo.
+    //G: You shall get away with this no more!
+    //R: Oh, it's you, my dear friend Gonzalo...
+    //R: It took you quite a while to find me, I was starting to lose hope.
+    //G: Mamma mia, can you believe it? On my way here, I found a giant pot filled with tomato salsa, and I accidentally dropped my keys in it, and had to dive in there to get them back--
+    //R: You
+    //R: You did what
+    //G: Huh?
+    //R: Huh?
+    //G: Well, whatever. That Spatula is mine and you know it, Ricardo!
+    //G: I shall take it back!
+    //R: Then perish.
+
+    //  Epilogue.
+    //
+}
