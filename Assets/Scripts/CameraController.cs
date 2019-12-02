dda25ad13b31b79601faa8e6612e5c57bd2eb988
Shirehii <daigoukaruna@gmail.com>
2019-12-02 17:58:33 +0000

    CCCCCCHANGES

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,10 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     private Vector3 offset = new Vector3(0, 0, -5);
 
+    void Start() 
+        {
+        player = GameObject.FindGameObjectWithTag("Player");
+        }
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
